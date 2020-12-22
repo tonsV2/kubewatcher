0b99337924eed912b460da3c8384b019c5188cc8
sj <sebastianthegreatful@gmail.com>
2020-12-22 14:22:35 +0700

    Format comment as desired by Intellij

--- kubewatcher.py
+++ kubewatcher.py
@@ -73,14 +73,14 @@ def yaml_path_extract_value(data, path) -> str:
 
 def yaml_rec(data, split_path, result):
     path_part = split_path.pop(0).strip()
-#    if len(split_path) == 0 and path_part.endswith("]"):
-#        find = path_part.find("[")
-#        index = path_part[find + 1:-1]
-#        path_part_without_index = path_part[0:find]
-#        if index == "*":
-#            result.append(data[path_part_without_index])
-#        else:
-#            result.append(data[path_part_without_index][int(index)])
+    # if len(split_path) == 0 and path_part.endswith("]"):
+    #     find = path_part.find("[")
+    #     index = path_part[find + 1:-1]
+    #     path_part_without_index = path_part[0:find]
+    #     if index == "*":
+    #         result.append(data[path_part_without_index])
+    #     else:
+    #         result.append(data[path_part_without_index][int(index)])
     if len(split_path) == 0:
         result.append(str(data[path_part]))
     else:
