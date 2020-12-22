29e9249fda3fabed4e6bec50d37d0a6650114fb3
sj <sebastianthegreatful@gmail.com>
2020-12-22 11:28:11 +0700

    Implement SMTP handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,11 @@
 import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from pprint import pprint
 
 import requests
+from ruamel import yaml
 
 from config import config
 
@@ -11,6 +15,33 @@ def handle(message, raw_object):
         response = post_message_to_slack(message)
         if not response['ok']:
             pprint(response)
+
+    if config['handlers']['smtp']:
+        send_mail(message, raw_object)
+
+
+# Inspiration: https://www.tutorialspoint.com/send-mail-from-your-gmail-account-using-python
+def send_mail(message, raw_object):
+    smtp_config = config['handlers']['smtp']
+    mail_message = MIMEMultipart()
+    mail_message['From'] = smtp_config['from']
+    mail_message['To'] = smtp_config['to']
+    mail_message['Subject'] = message
+
+    body = yaml.safe_dump(raw_object)
+
+    mail_message.attach(MIMEText(body, 'plain'))
+
+    session = smtplib.SMTP(smtp_config['host'], smtp_config['port'])
+    if smtp_config['tls']:
+        session.starttls()
+    session.login(smtp_config['from'], smtp_config['password'])
+    text = mail_message.as_string()
+    try:
+        session.sendmail(smtp_config['from'], smtp_config['to'], text)
+    except smtplib.SMTPAuthenticationError as exc:
+        print(exc)
+    session.quit()
 
 
 # Inspiration: https://keestalkstech.com/2019/10/simple-python-code-to-send-message-to-slack-channel-without-packages/
