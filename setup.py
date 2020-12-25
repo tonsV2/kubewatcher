0f845708ddae52bd999d9d7434c3462fce8b6766
sj <sebastianthegreatful@gmail.com>
2020-12-25 14:55:45 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name='kubewatcher',
-    version='1.3.0',
+    version='1.4.0',
     description='See https://github.com/tonsV2/kubewatcher',
     python_requires='>=3.8',
     packages=find_packages(),
