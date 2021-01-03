cbf01024be8058e195a4125e9e3932239ca9240c
sj <sebastianthegreatful@gmail.com>
2021-01-03 22:18:30 +0700

    Test path_extractor.extract_values

---
+++ tests/test_path_extractor__extract_values.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+
+from kubewatcher.path_extractor import extract_values
+
+
+class Test(TestCase):
+    def test_extract_values__simple_property(self):
+        data = {
+            "property": "value"
+        }
+
+        path = "property"
+
+        actual_value = extract_values(data, path)
+
+        self.assertEqual(actual_value, ["value"])
+
+    def test_extract_values__nested_property(self):
+        data = {
+            "property": {
+                "property": "value"
+            }
+        }
+
+        path = "property.property"
+
+        actual_value = extract_values(data, path)
+
+        self.assertEqual(actual_value, ["value"])
+
+    def test_extract_values__multiple_values(self):
+        data = {
+            "property": [
+                {"state": "value0"},
+                {"state": "value1"}
+            ]
+        }
+
+        path = "property[*].state"
+
+        actual_value = extract_values(data, path)
+
+        self.assertEqual(actual_value, ["value0", "value1"])
+
+    def test_extract_values__multiple_nested_values(self):
+        data = {
+            "property": [
+                {
+                    "property": {
+                        "state": "value0"
+                    }
+                },
+                {
+                    "property": {
+                        "state": "value1"
+                    }
+                },
+            ]
+        }
+
+        path = "property[*].property.state"
+
+        actual_value = extract_values(data, path)
+
+        self.assertEqual(actual_value, ["value0", "value1"])
