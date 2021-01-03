1ba1204b91738c27dfd450a38d3dd396d244c9a4
sj <sebastianthegreatful@gmail.com>
2021-01-03 20:11:22 +0700

    Test Telegram handler

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -2,7 +2,7 @@ import time
 from unittest import TestCase
 from unittest.mock import patch
 
-from kubewatcher.handlers import handle
+from kubewatcher.handlers import handle, post_message_to_telegram
 
 
 class Test(TestCase):
@@ -40,3 +40,24 @@ class Test(TestCase):
         post_message_to_slack_mock.assert_not_called()
         send_mail_mock.assert_not_called()
         post_message_to_telegram_mock.assert_not_called()
+
+    @patch("requests.get")
+    def test_handle__post_message_to_telegram(self, get_mock):
+        token = "token"
+        chat_id = "chat-id"
+
+        config = {
+            "handlers": {
+                "telegram": {
+                    "token": token,
+                    "chatId": chat_id
+                }
+            }
+        }
+
+        message = "message"
+
+        post_message_to_telegram(config, message)
+
+        url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}"
+        get_mock.assert_called_with(url)
