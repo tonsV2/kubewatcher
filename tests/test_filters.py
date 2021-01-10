90333e893025cda201f94a615e53499e2e7b0e35
sj <sebastianthegreatful@gmail.com>
2021-01-10 14:43:01 +0700

    Add testing of filters. It's now possible to associate test data with filters and thereby ensure they work as intended

---
+++ tests/test_filters.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from envyaml import EnvYAML
+
+from kubewatcher.kubewatcher import read_configs
+from kubewatcher.kubewatcher import trigger
+
+
+class Test(TestCase):
+    def test_filters(self):
+        config_file = "../config.yaml"
+        config = read_configs([config_file])
+
+        for f in config['filters']:
+            if 'tests' in f:
+                for test in f['tests']:
+                    data = EnvYAML(test).export()
+                    triggered = trigger(f, data)
+                    if not triggered:
+                        print(f)
+                    self.assertTrue(triggered)
+
+    def test_ensure_all_filters_are_tested(self):
+        config_file = "../config.yaml"
+        config = read_configs([config_file])
+
+        for f in config['filters']:
+            missing_tests = 'tests' not in f
+            if missing_tests:
+                print(f)
+            self.assertFalse(missing_tests)
